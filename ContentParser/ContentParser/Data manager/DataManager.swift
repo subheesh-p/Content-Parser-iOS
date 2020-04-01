c9061bf6ae3f55da911a257c0cdd294047e2052b
Subheesh <subheesh@qburst.com>
2020-04-01 13:42:18 +0530

    Added data manager and constants classes

---
+++ ContentParser/ContentParser/Data manager/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  ContentParser
+//
+//  Created by Subeesh on 01/04/20.
+//  Copyright © 2020 qburst. All rights reserved.
+//
+
+import UIKit
+
+class DataManager {
+
+}
---
+++ ContentParser/ContentParser/Utility/Constants/AppConstants.swift
@@ -0,0 +1,14 @@
+//
+//  AppConstants.swift
+//  ContentParser
+//
+//  Created by Subeesh on 01/04/20.
+//  Copyright © 2020 qburst. All rights reserved.
+//
+
+import UIKit
+
+struct AppConstants {
+
+    static let dataUrlString = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
+}
